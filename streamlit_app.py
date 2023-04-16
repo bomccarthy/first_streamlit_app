62171d6d717d171d2f3f171fc549ee61f15ea4f3
bomccarthy <110850293+bomccarthy@users.noreply.github.com>
2023-04-16 15:05:52 -0500

    Update streamlit_app.py
    Removed json from display by commenting out line 28

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # Writes data to the screen
+# streamlit.text(fruityvice_response.json()) # Writes data to the screen
 
 # Normalizes the json response from above 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
